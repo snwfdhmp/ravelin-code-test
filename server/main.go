29e1f327bca2a74f46656de4bf71538c87ecdb56
George Thomas <georgetamayothomas@gmail.com>
2016-04-20 14:07:23 +0100

    Add server starting point

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	fmt.Println("Server now running on localhost:8080")
+	fmt.Println(`Try running: curl -X POST -d '{"hello":"test123"}' http://localhost:8080/helloworld`)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to read body"))
+		return
+	}
+
+	type helloWorldRequest struct {
+		Hello string `json:"hello"`
+	}
+
+	req := &helloWorldRequest{}
+
+	if err = json.Unmarshal(body, req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to unmarshal JSON request"))
+		return
+	}
+
+	resp := make(map[string]interface{})
+	resp["yourHello"] = req.Hello
+
+	out, _ := json.Marshal(resp)
+
+	w.Write(out)
+}
